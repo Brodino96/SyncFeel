9a9478fdadbc5dd1046dc628f866959b7f97cadf
Brodino <brodino96@gmail.com>
2024-06-18 18:21:11 +0200

    Changed layout... yes i'm acustic

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -6,25 +6,14 @@ author "Brodino"
 descritpion "Sync what happens inside the game with your sex toys"
 version "1.0"
 
-shared_scripts {
-    "@ox_lib/init.lua",
-    "config.lua",
-    "locale/*",
-}
+shared_scripts { "@ox_lib/init.lua", "config.lua", "locale/*" }
 
-server_scripts {
-    "server/*",
-}
-
-client_scripts {
-    "client/*",
-}
+server_scripts { "server/*" }
+client_scripts { "client/*" }
 
 ui_page "web/index.html"
 files { "web/*" }
 
 dependency "ox_lib"
 
-escrow_ignore {
-    "config.lua"
-}
+escrow_ignore { "config.lua" }
